025f774fc5aece799d6ed47ee2a84f8c34d24cf2
oakpunonu <147940216+oakpunonu@users.noreply.github.com>
2026-05-30 21:03:09 +0100

    Add files via upload

---
+++ Week-9/Inventory.java
@@ -0,0 +1,13 @@
+public class Inventory {
+    boolean checkStock(String productId) {
+        return true;
+    }
+
+    void reserve(String productId) {
+        System.out.println("Reserved " + productId);
+    }
+
+    void release(String productId) {
+        System.out.println("Released " + productId);
+    }
+}
---
+++ Week-9/Shipping.java
@@ -0,0 +1,13 @@
+public class Shipping {
+    String createLabel(String address) {
+        return "TRK" + System.currentTimeMillis();
+    }
+
+    void schedulePickup(String label) {
+        System.out.println("Pickup scheduled: " + label);
+    }
+
+    boolean isAvailable() {
+        return true;
+    }
+}
